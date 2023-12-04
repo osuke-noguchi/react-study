701b464d6b49809e7fe61bea69e5800ce443a6dd
osuke-noguchi <noguchi.nodai@gmail.com>
2023-12-05 06:06:27 +0900

    stateを渡すページ遷移

--- src/UrlParameter.jsx
+++ src/UrlParameter.jsx
@@ -1,11 +1,15 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useLocation } from 'react-router-dom';
 
 export const UrlParameter = () => {
   const { id } = useParams();
+  const { search } = useLocation();
+  const query = new URLSearchParams(search);
+
   return (
     <div>
       <h1>UrlParameterのページです</h1>
       <p>{id}です</p>
+      <p>クエリパラメータは{query.get('name')}です</p>
     </div>
   );
 };
